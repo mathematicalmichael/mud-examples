68c17073a58703c80ee5e7d9cda53510ffc9d199
Michael Pilosov <consistentbayes@gmail.com>
2021-02-12 06:38:45 +0000

    ode and pde refactored into separate files

--- scripts/mud_problem.py
+++ scripts/mud_problem.py
@@ -1,20 +1,8 @@
 import numpy as np
 
-from mud.plot import make_2d_unit_mesh
-from mud.util import std_from_equipment
-
 from mud_examples.plotting import plot_experiment_measurements, plot_experiment_equipment
-from mud_examples.plotting import plot_decay_solution, plot_poisson_solution
+from mud_examples.plotting import plot_poisson_solution
 
-
-from mud_examples.plotting import fit_log_linear_regression
-from mud_examples.models import generate_decay_model
-from mud_examples.models import generate_temporal_measurements as generate_sensors_ode
-from mud_examples.models import generate_spatial_measurements as generate_sensors_pde
-from mud_examples.datasets import load_poisson
-
-from mud.funs import mud_problem, map_problem
-from mud_examples.helpers import experiment_measurements, extract_statistics, experiment_equipment
 
 import pickle
 import matplotlib
@@ -24,197 +12,8 @@ matplotlib.backend = 'Agg'
 matplotlib.rcParams['figure.figsize'] = 10,10
 matplotlib.rcParams['font.size'] = 16
 
-### PDE ###
-def main_pde(model_list, num_trials=5,
-             tolerances=[],
-             measurements=[],
-             fsize=32,
-             seed=21,
-             lam_true=3.0,
-             input_dim=2,
-             domain=[[-4,0]], alt=False, bayes=False):
-    import poisson as ps  # TODO: move this to mud_examples
-    print(f"Will run simulations for N={measurements}")
-    res = []
-    num_measure = max(measurements)
-
-    sd_vals     = [ std_from_equipment(tolerance=tol, probability=0.99) for tol in tolerances ]
-    sigma       = sd_vals[-1] # sorted, pick largest
-    example_list = [ 'mud' ]
-    if alt:
-        example_list.append('mud-alt')
-    if bayes:
-        example_list.append('map')
-
-    for example in example_list:
-        print(f"Example: {example}")
-        P = ps.pdeProblem()
-        # in 1d this is a change in sensor location
-        # in ND, change in how we partition sensors (vertical vs horizontal)
-        if example == 'mud-alt' and input_dim == 1:
-            fname = 'pde_{input_dim}D/mud-alt_ref.pkl'
-            try:
-                P.load(fname)
-            except FileNotFoundError:
-                # attempt to load xml results from disk.
-                ps.make_reproducible_without_fenics('mud-alt', lam_true, input_dim=1, num_samples=None, num_measure=num_measure)
-                P.load(fname)
-            wrapper = P.mud_scalar()
-            ps.plot_without_fenics(fname, num_sensors=100, num_qoi=1, example=example)
-        else:
-            fname = f'pde_{input_dim}D/mud_ref.pkl'
-            try:
-                P.load(fname)
-            except FileNotFoundError:
-                ps.make_reproducible_without_fenics('mud', lam_true, input_dim=input_dim, num_samples=None, num_measure=num_measure)
-                P.load(fname)
-
-            if example == 'mud-alt':
-                wrapper = P.mud_vector_vertical()
-                ps.plot_without_fenics(fname, num_sensors=100, mode='ver',
-                                       num_qoi=input_dim, example=example)
-            else:
-                wrapper = P.mud_vector_horizontal()
-                ps.plot_without_fenics(fname, num_sensors=100, mode='hor',
-                                       num_qoi=input_dim, example=example)
-
-        # adjust measurements to account for what we actually have simulated
-        measurements = np.array(measurements)
-        measurements = list(measurements[measurements <= P.qoi.shape[1]])
-        print("Increasing Measurements Study")
-        experiments, solutions = experiment_measurements(num_measurements=measurements,
-                                                 sd=sigma,
-                                                 num_trials=num_trials,
-                                                 seed=seed,
-                                                 fun=wrapper)
-
-        means, variances = extract_statistics(solutions, lam_true)
-        regression_mean, slope_mean = fit_log_linear_regression(measurements, means)
-        regression_vars, slope_vars = fit_log_linear_regression(measurements, variances)
-
-        ##########
-
-        num_sensors = min(100, num_measure)
-        if len(tolerances) > 1:
-            print("Increasing Measurement Precision Study")
-            sd_means, sd_vars = experiment_equipment(num_trials=num_trials,
-                                                  num_measure=num_sensors,
-                                                  sd_vals=sd_vals,
-                                                  reference_value=lam_true,
-                                                  fun=wrapper)
-
-            regression_err_mean, slope_err_mean = fit_log_linear_regression(tolerances, sd_means)
-            regression_err_vars, slope_err_vars = fit_log_linear_regression(tolerances, sd_vars)
-            _re = (regression_err_mean, slope_err_mean,
-                   regression_err_vars, slope_err_vars,
-                   sd_means, sd_vars, num_sensors)
-        else:
-            _re = None  # hack to avoid changing data structures for the time being
-
-        _in = (P.lam, P.qoi, P.sensors, P.qoi_ref, experiments, solutions)
-        _rm = (regression_mean, slope_mean, regression_vars, slope_vars, means, variances)
-        res.append((example, _in, _rm, _re))
-
-        if input_dim > 1:
-            P.plot_initial()
-            P.plot_solutions(solutions, 20, example=example)
-            P.plot_solutions(solutions, 100, example=example, save=True)
-
-    return res
-
-
-### ODE ###
-def main_ode(num_trials,
-             fsize=32,
-             seed=21,
-             lam_true=0.5,
-             domain=[[0,1]],
-             tolerances=[0.1],
-             time_ratios=[1], alt=False, bayes=False):
-    res = []
-    print(f"Will run simulations for %T={time_ratios}")
-    sd_vals      = [ std_from_equipment(tolerance=tol, probability=0.99) for tol in tolerances ]
-    sigma        = sd_vals[-1] # sorted, pick largest
-    t_min, t_max = 1, 3
-    example_list = [ 'ode-mud' ]
-    if alt:
-        example_list.append('ode-mud-alt')
-
-    if bayes:
-        example_list.append('ode-map')
-
-    for example in example_list:
-        print(f"Example: {example}")
-        if example == 'ode-mud-alt':
-            sensors = generate_sensors_ode(measurement_hertz=200, start_time=t_min, end_time=t_max)
-        else:
-            sensors = generate_sensors_ode(measurement_hertz=100, start_time=t_min, end_time=t_max)
-
-        measurements = [ int(np.floor(len(sensors)*r)) for r in time_ratios ]
-        print(f"Measurements: {measurements}")
-#         times        = [ sensors[m-1] for m in measurements ]
-        num_measure = max(measurements)
-
-        model    = generate_decay_model(sensors, lam_true)
-        qoi_true = model() # no args evaluates true param
-        np.random.seed(seed)
-        lam = np.random.rand(int(1E3)).reshape(-1,1)
-        qoi = model(lam)
-
-        if example == 'ode-map':
-            def wrapper(num_obs, sd):
-                return map_problem(domain=domain, lam=lam, qoi=qoi,
-                                   sd=sd, qoi_true=qoi_true, num_obs=num_obs)
-        else:
-            def wrapper(num_obs, sd):
-                return mud_problem(domain=domain, lam=lam, qoi=qoi,
-                                   sd=sd, qoi_true=qoi_true, num_obs=num_obs)
-
-
-        print("Increasing Measurements Quantity Study")
-        experiments, solutions = experiment_measurements(num_measurements=measurements,
-                                                 sd=sigma,
-                                                 num_trials=num_trials,
-                                                 seed=seed,
-                                                 fun=wrapper)
-
-        means, variances = extract_statistics(solutions, lam_true)
-        regression_mean, slope_mean = fit_log_linear_regression(time_ratios, means)
-        regression_vars, slope_vars = fit_log_linear_regression(time_ratios, variances)
-
-        ##########
-
-        num_sensors = num_measure
-        if len(tolerances) > 1:
-            
-            print("Increasing Measurement Precision Study")
-            sd_means, sd_vars = experiment_equipment(num_trials=num_trials,
-                                                  num_measure=num_sensors,
-                                                  sd_vals=sd_vals,
-                                                  reference_value=lam_true,
-                                                  fun=wrapper)
-
-            regression_err_mean, slope_err_mean = fit_log_linear_regression(tolerances, sd_means)
-            regression_err_vars, slope_err_vars = fit_log_linear_regression(tolerances, sd_vars)
-            _re = (regression_err_mean, slope_err_mean,
-                   regression_err_vars, slope_err_vars,
-                   sd_means, sd_vars, num_sensors)
-        else:
-            _re = None  # hack to avoid changing data structures for the time being
-
-        # TO DO clean all this up
-        _in = (lam, qoi, sensors, qoi_true, experiments, solutions)
-        _rm = (regression_mean, slope_mean, regression_vars, slope_vars, means, variances)
-        example_name = '-'.join(example.split('-')[1:]).upper()
-        res.append((example_name, _in, _rm, _re))
-
-        # TODO check for existence of save directory, grab subset of measurements properly.
-        plot_decay_solution(solutions, generate_decay_model, fsize=fsize,
-                            end_time=t_max, lam_true=lam_true, qoi_true=qoi_true,
-                            sigma=sigma, time_vector=sensors, prefix='ode/' + example)
-
-    return res
-
+from ode import main_ode
+from pde import main_pde
 
 def main(args):
     np.random.seed(args.seed)
@@ -242,8 +41,7 @@ def main(args):
     print("Running...")
     if example == 'pde':
         lam_true = 3.0
-        model_list  = pickle.load(open(f'res{inputdim}u.pkl', 'rb'))
-        res = main_pde(model_list, num_trials=num_trials,
+        res = main_pde(num_trials=num_trials,
                          fsize=fsize,
                          seed=seed,
                          lam_true=lam_true,
