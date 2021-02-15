96bc87fa06247e08875eebf2be14c6dcdf42e2f6
Michael Pilosov <consistentbayes@gmail.com>
2021-02-15 05:17:36 +0000

    try failing import

--- src/mud_examples/helpers.py
+++ src/mud_examples/helpers.py
@@ -31,7 +31,10 @@ class LazyLoader(types.ModuleType):
     return self._mod
 
   def __getattr__(self, attrb):
-    return getattr(self._load(), attrb)
+    try:
+        return getattr(self._load(), attrb)
+    except ModuleNotFoundError:
+        pass
 
   def __dir__(self):
     return dir(self._load())
@@ -97,40 +100,6 @@ def extract_statistics(solutions, reference_value):
         means.append(mean_mud_sol)
         variances.append(var_mud_sol)
 
-    return means, variances
-
-
-def experiment_measurements_index(fun, num_measurements, sd, num_trials, seed=21):
-    """
-    Fixed sensors, varying how much data is incorporated into the solution.
-    """
-    experiments = {}
-    solutions = {}
-    for ns in num_measurements:
-        ratios = []
-        mud_solutions = []
-        for t in range(num_trials):
-            np.random.seed(seed+t)
-            _r = fun(sd=sd, num_obs=ns)
-            ratios.append(_r)
-            mud_solutions.append(np.argmax(_r))
-        experiments[ns] = ratios
-        solutions[ns] = mud_solutions
-    
-    return experiments, solutions
-
-
-def extract_statistics_index(x, solutions, reference_value):
-    num_sensors_plot_conv = solutions.keys()
-    means = []
-    variances = []
-    for ns in num_sensors_plot_conv:
-        mud_solutions = [ x[_index] for _index in solutions[ns] ]
-        err = np.linalg.norm(np.array(mud_solutions) - reference_value, axis=1)/np.sqrt(len(reference_value))
-        mean_mud_sol = np.mean(err)
-        var_mud_sol = np.var(err)
-        means.append(mean_mud_sol)
-        variances.append(var_mud_sol)
     return means, variances
 
 
