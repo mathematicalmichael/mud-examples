1a061331fef3d81ab3e0fb253fded46ef8462fc2
Michael Pilosov <consistentbayes@gmail.com>
2021-03-15 04:45:06 +0000

    generate data

--- scripts/generate_pde_data.sh
+++ scripts/generate_pde_data.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 NUM_SAMPLES=1000
-TOL=95
-# FILE_PREFIX=${TOL}results
-# DIST=n
 FILE_PREFIX=results
-DIST=u
-for dim in 2 ; do
+DIST=n
+DIM=2
+for TOL in 95 99 9999; do
     echo "Running for Dim=${dim}."
-    generate_poisson_data -v -n ${NUM_SAMPLES} -d ${DIST} -t 0.${TOL} -i ${dim} -p ${FILE_PREFIX}
+    generate_poisson_data -v -s "$NUM_SAMPLES" -d "$DIST" -t 0.${TOL} -i "$DIM" -m -2.0 || break
+done
+
+DIST=u
+for DIM in 2 5 11; do
+    echo "Running for Dim=${dim}."
+    generate_poisson_data -v -s "$NUM_SAMPLES -d "$DIST" -i "$DIM" || break
 done
