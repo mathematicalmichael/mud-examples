d2a8cd61e245290e6f173cde294a012926315d7a
Michael Pilosov <consistentbayes@gmail.com>
2021-03-15 07:50:45 +0000

    segment out data generation

--- scripts/generate_pde_data_normal.sh
+++ scripts/generate_pde_data_normal.sh
@@ -2,7 +2,7 @@
 NUM_SAMPLES=1000
 DIST=n
 DIM=2
-for TOL in 95 99 9999; do
+for TOL in 95 99; do
     echo "Running with Normal for Tolerance=0.${TOL}."
     generate_poisson_data -v -s "$NUM_SAMPLES" -d "$DIST" -t 0.${TOL} -i "$DIM" -m -2.0 || break
 done
