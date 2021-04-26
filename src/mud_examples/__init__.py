7ed4a842978aa2d52cf2c92a7d1e872d9a2425d0
Michael Pilosov <consistentbayes@gmail.com>
2021-04-25 21:29:20 -0600

    Revert "try forcing dist name"
    This reverts commit 175db3f6db65625f7202e4c5bdbab67823e201a5.

--- src/mud_examples/__init__.py
+++ src/mud_examples/__init__.py
@@ -9,7 +9,6 @@ else:
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = __name__
-    dist_name = 'mud_examples'
     __version__ = version(dist_name)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
